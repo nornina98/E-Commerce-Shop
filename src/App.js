d39c3353a883f630bf02e7bb1586879bf4ada4a2
IKRAM-LAPTOP <nornina8998@gmail.com>
2022-06-01 01:16:16 +0800

    updated npm and add outlet for consistent nagivation bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import Home from "./components/routes/home/home-component";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>This Is Navigation</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    //Routes URL that passing component
+    //Routes URL that passing component and it's nested!
     <Routes>
-      <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };
