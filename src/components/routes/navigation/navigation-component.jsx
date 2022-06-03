20c60a5d8faf157291cd7932394428fd735a5ca8
IKRAM-LAPTOP <nornina8998@gmail.com>
2022-06-03 19:57:36 +0800

    create assets folder, nav scss, import svg logo

--- src/components/routes/navigation/navigation-component.jsx
+++ src/components/routes/navigation/navigation-component.jsx
@@ -1,14 +1,21 @@
 import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
 
+// Import CrownLogo in Assets
+import { ReactComponent as CrownLogo } from "../../../assets/crown.svg";
+
+// import styling from scss within same directory
+import "./navigation-styles.scss";
+
 const Navigation = () => {
+  // use fragment instead of wrapping div as cointaner
   return (
     <Fragment>
       <div className="navigation">
-        <Link className="nav-link-logo" to="/">
-          <div className="logo">LOGO</div>
+        <Link className="logo-container" to="/">
+          <CrownLogo className="logo " />
         </Link>
-        <div className="nav-link-container">
+        <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
