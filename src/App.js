a0746072cefa787952eb8a8f3dea628becd4da2a
IKRAM-LAPTOP <nornina8998@gmail.com>
2022-06-27 10:07:28 +0800

    added shop component and setup new shop routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/routes/home/home-component.jsx";
 import Navigation from "./components/routes/navigation/navigation-component.jsx";
 import Authentication from "./components/routes/authentication/authentication-component.jsx";
+import Shop from "./components/routes/shop/shop-component.jsx";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="/auth" element={<Authentication />} />
+        <Route path="/shop" element={<Shop />} />
       </Route>
     </Routes>
   );
