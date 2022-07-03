cda9e646b25819e2dcaff1238295cf376c616a25
IKRAM-LAPTOP <nornina8998@gmail.com>
2022-07-04 02:37:49 +0800

    added checkout item component and it's styles .scss

--- src/components/routes/checkout/checkout-component.jsx
+++ src/components/routes/checkout/checkout-component.jsx
@@ -2,6 +2,8 @@ import { useContext } from "react";
 
 import { CartContext } from "../../../contexts/context-cart";
 
+import CheckoutItem from "../../checkout-item/checkout-item-component";
+
 import "./checkout-styles.scss";
 
 const Checkout = () => {
@@ -9,23 +11,28 @@ const Checkout = () => {
     useContext(CartContext);
 
   return (
-    <div>
-      <h1>Checkout Page</h1>
-      <div>
-        {cartItems.map((cartItem) => {
-          const { id, name, quantity } = cartItem;
-          return (
-            <div key={id}>
-              <h2>{name}</h2>
-              <span>{quantity}</span>
-              <br />
-              <span onClick={() => addItemToCart(cartItem)}>increment</span>
-              <br />
-              <span onClick={() => removeItemToCart(cartItem)}>decrement</span>
-            </div>
-          );
-        })}
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
       </div>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <span className="total">Total: 0</span>
     </div>
   );
 };
