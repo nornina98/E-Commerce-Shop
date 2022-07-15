e12ea39e85d0cd9c5a2f0f656c5f4047a9ffe462
IKRAM-LAPTOP <nornina8998@gmail.com>
2022-07-15 11:48:20 +0800

    added preview component and limit it as 4 card

--- src/components/routes/shop/shop-component.jsx
+++ src/components/routes/shop/shop-component.jsx
@@ -1,24 +1,20 @@
-import { useContext, Fragment } from "react";
+import { useContext } from "react";
 import { CategoriesContext } from "../../../contexts/categories-context";
-import ProductCard from "../../product-card/product-card-component";
+import CategoryPreview from "../../category-preview/category-preview-component";
 
 import "./shop-styles.scss";
 
 const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
+    <div className="shop-cointainer">
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </div>
   );
 };
 
