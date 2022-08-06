880a0a05b6752401a0cd0ff540ab6964e288e1af
IKRAM-LAPTOP <nornina8998@gmail.com>
2022-08-07 01:19:41 +0800

    added user selector

--- src/contexts/context-user.jsx
+++ src/contexts/context-user.jsx
@@ -35,7 +35,6 @@ const INITIAL_STATE = {
 
 export const UserProvider = ({ children }) => {
   const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
-  console.log(currentUser);
 
   const setCurrentUser = (user) => {
     dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
