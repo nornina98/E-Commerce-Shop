008b062c9d094d53ac364d1af3b8d79dce1580e6
IKRAM-LAPTOP <nornina8998@gmail.com>
2022-10-10 14:06:38 +0800

    setup stripe payment form and config

--- src/components/payment-form/payment-form-component.jsx
+++ src/components/payment-form/payment-form-component.jsx
@@ -7,8 +7,22 @@ import {
 } from "./payment-form-styles";
 
 const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
   const paymentHandler = async (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: 100000 }),
+    }).then((res) => res.json());
+    console.log(response);
   };
 
   return (
