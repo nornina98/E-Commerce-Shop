6d976821ce4b9989a0a42d9e161456063fd11671
Nor Ikram <nornina8998@gmail.com>
2022-10-12 11:20:58 +0800

    add spinner in payment card button

--- src/components/payment-form/payment-form-component.jsx
+++ src/components/payment-form/payment-form-component.jsx
@@ -1,3 +1,9 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
+import { selectCartTotal } from "../../store/cart/cart-selector";
+import { selectCurrentUser } from "../../store/user/user-selector";
+
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import { BUTTON_TYPE_CLASSES } from "../button/button-component";
 import {
@@ -9,20 +15,43 @@ import {
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const amount = useSelector(selectCartTotal);
+  const currentUser = useSelector(selectCurrentUser);
+  const [isProcessingPayment, setIsProcessingPayment] = useState(false);
 
   const paymentHandler = async (e) => {
     e.preventDefault();
     if (!stripe || !elements) {
       return;
     }
+    setIsProcessingPayment(true);
     const response = await fetch("/.netlify/functions/create-payment-intent", {
       method: "post",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ amount: 100000 }),
+      body: JSON.stringify({ amount: amount * 100 }),
     }).then((res) => res.json());
     console.log(response);
+
+    const clientSecret = response.paymentIntent.client_secret;
+    const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: currentUser ? currentUser.displayName : "Guest",
+        },
+      },
+    });
+
+    setIsProcessingPayment(false);
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment Successful");
+      }
+    }
   };
 
   return (
@@ -30,7 +59,10 @@ const PaymentForm = () => {
       <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment:</h2>
         <CardElement />
-        <PaymentButton buttonType={BUTTON_TYPE_CLASSES.inverted}>
+        <PaymentButton
+          buttonType={BUTTON_TYPE_CLASSES.inverted}
+          isLoading={isProcessingPayment}
+        >
           Pay Now
         </PaymentButton>
       </FormContainer>
